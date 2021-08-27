fdd67c67cc28b5383fc4698dd34c7fa6d0d691d5
Parth Gandhi <parthgandhi@outlook.com>
2021-08-27 12:48:15 +0530

    add some responsiveness

--- pages/statsPreview.tsx
+++ pages/statsPreview.tsx
@@ -7,35 +7,44 @@ import { NextPage } from "next";
 const StatsPreviewCard: NextPage = () => {
   return (
     <div className="bg-gray-900 w-screen h-screen flex items-center justify-center">
-      <div className="grid grid-cols-2 max-w-4xl rounded-xl overflow-hidden h-80">
-        <div className="col-span-1 p-10 bg-gray-800">
-          <h1 className="text-gray-100 text-3xl font-semibold">
+      <div className="grid grid-cols-1 grid-rows-3 w-3/4 sm:max-w-xs lg:grid-rows-1 lg:grid-cols-2 lg:max-w-4xl rounded-xl overflow-hidden h-3/4 lg:max-h-80">
+        <div className="lg:hidden row-span-1">
+          <img
+            src="http://placekitten.com/300/300"
+            alt="kitten placeholder"
+            className="h-full w-full object-center object-cover"
+          />
+        </div>
+        <div className="lg:col-span-1 p-3 lg:p-10 bg-gray-800 row-span-2">
+          <h1 className="text-gray-100 sm:text-3xl font-semibold text-l">
             Get <span className="text-purple-600">insights</span> that help your
             business grow.
           </h1>
-          <p className="text-purple-100 leading-[1.375rem] text-sm font-extralight mt-4">
+          <p className="text-purple-100 leading-[1.375rem] text-xs sm:text-sm font-extralight mt-2">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
             rhoncus sapien finibus erat euismod interdum.
           </p>
-          <div className="grid grid-cols-3 mt-10">
+          <div className="grid grid-rows-3 sm:grid-cols-3 mt-2 sm:mt-10">
             <div className="col-span-1">
-              <span className="text-gray-200 font-bold tracking-wide">
+              <span className="text-gray-200 font-bold tracking-wide text-xs sm:text-base">
                 10k+
               </span>
               <br />
-              <span className="text-gray-300 text-xs font-thin tracking-widest">
+              <span className="text-gray-300 text-xs font-thin tracking-widest uppercase">
                 companies
               </span>
             </div>
             <div className="col-span-1 uppercase">
-              <span className="text-gray-200 font-bold tracking-wide">314</span>
+              <span className="text-gray-200 font-bold tracking-wide text-xs sm:text-base">
+                314
+              </span>
               <br />
               <span className="text-gray-300 text-xs font-thin tracking-widest">
                 templates
               </span>
             </div>
             <div className="col-span-1 uppercase">
-              <span className="text-gray-200 font-bold tracking-wide">
+              <span className="text-gray-200 font-bold tracking-wide text-xs sm:text-base">
                 12M+
               </span>
               <br />
@@ -45,8 +54,7 @@ const StatsPreviewCard: NextPage = () => {
             </div>
           </div>
         </div>
-        <div className="col-span-1">
-          {/** why don't object-cover object-center work here? */}
+        <div className="col-span-1 hidden lg:block relative">
           <img
             src="http://placekitten.com/300/300"
             alt="kitten placeholder"
