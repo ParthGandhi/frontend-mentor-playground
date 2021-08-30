472f9906fdb66b3cb5e9187dd6b3334932554c61
Parth Gandhi <parthgandhi@outlook.com>
2021-08-30 12:45:20 +0530

    setup fylo assets

---
+++ pages/fylo.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+
+/**
+ * https://www.frontendmentor.io/challenges/fylo-dark-theme-landing-page-5ca5f2d21e82137ec91a50fd
+ */
+const FyloLandingPage: NextPage = () => {
+  return <div></div>;
+};
+
+export default FyloLandingPage;
